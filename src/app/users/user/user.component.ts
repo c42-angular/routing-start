eb115fe2828107f3c6f288a56e66646b60733d41
arun@win10.bootcamp.com <arun@win10.bootcamp.com>
2017-08-22 23:24:36 +0100

    1. passing params to routes & reading them inside components; 2.specifying relative path kn router.navigate(); 3. route params as observables

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,24 @@ import { Component, OnInit } from '@angular/core';
 export class UserComponent implements OnInit {
   user: {id: number, name: string};
 
-  constructor() { }
+  constructor(private activeRoute: ActivatedRoute) { }
 
   ngOnInit() {
+    // reading params from active route
+    this.user = {
+      id: this.activeRoute.snapshot.params['id'],
+      name: this.activeRoute.snapshot.params['name']
+    }
+
+    // route params as observables
+    this.activeRoute.params.subscribe(
+      (p) => {
+        this.user = {
+          id: p['id'],
+          name: p['name'],
+        }
+      }
+    );
   }
 
 }
