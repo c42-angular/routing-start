197d20301f9410fb3254a1db141c1842cad3e3c9
Arunkumar Ramachandriya <arun@Arunkumars-MacBook-Pro.local>
2017-08-24 16:08:13 +0100

    Auth Guard redirecting to home page if no access (demonstrates async nature of Promise too by navigating away in 800ms)

--- src/app/auth-guard.service.ts
+++ src/app/auth-guard.service.ts
@@ -1,4 +1,4 @@
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
 import { Injectable } from '@angular/core';
 
@@ -7,14 +7,18 @@ import { AuthService } from 'app/auth.service';
 @Injectable()
 export class AuthGuard implements CanActivate {
 
-  constructor(private authService: AuthService) { }
+  constructor(private authService: AuthService, private router: Router) { }
 
   canActivate(route: ActivatedRouteSnapshot,
               state: RouterStateSnapshot): boolean | Observable<boolean> | Promise<boolean> {
     return this.authService.isAuthenticated()
       .then(
         (authenticated: boolean) => {
-          return authenticated;
+          if (authenticated) {
+            return true;
+          } else {
+            this.router.navigate(['/']);
+          }
         }
       );
   }
